30c76129c8633c61076e84977a571eab5433b9cb
Konrad <konradzuse.dm@gmail.com>
2021-05-10 18:29:42 -0400

    completed geting info from database OOP PHP

--- t/class.php
+++ t/class.php
@@ -1,4 +1,78 @@
 <?php
+      // ------- Store MySQL server global configuration --------------------------- 
+      $conf=[
+              'serv' => 'localhost',
+              'user' => 'konrad',
+              'pass' => 'Hall0w33nday',
+              'dbnm' => 'blog'
+            ];
+      // ----------------------------------------------------------------------------
+
+      class DBwrapper
+      {
+        private $srv; // serverhost
+        private $usr; // dbUser
+        private $key; // dbPass
+        private $db;  // dbName
+        private $con; // dbConnection
         
+
+        // Constructor Initialice the Object instanceof
+              public function __construct($config)
+              {
+                $this->srv  = $config['serv'];
+                $this->usr  = $config['user'];
+                $this->key  = $config['pass'];
+                $this->db   = $config['dbnm'];
+              
+                  $this->con = new mysqli($this->srv,$this->usr,$this->key,$this->db); // host user pass dbName
+                  if(mysqli_connect_error())
+                  {
+                    trigger_error("Error connecting to database:".mysqli_connect_error());
+                  }else
+                       {
+                        return $this->con; 
+                       }
+              }
+
+        // Fetch Database information method
+              public function getData()
+              {
+                // MySQL Query string
+                // the database fields are id	name	email	username	password
+                    $sql = "SELECT * FROM `blog`.`customers`"; 
+                                               // New Empty array to store results of the qry
+                    
+                    $result=$this->con->query($sql);
+                    
+                      if($result->num_rows)
+                      {
+                        $data = array();
+                          while(($row=$result->fetch_assoc()))
+                          {   
+                            $data[]=$row;
+                          }
+                        return $data;  
+                      }else
+                           {
+                              return NULL;
+                           }
+                  
+              }
+
+      }  
+     
+
+      $myObj = new DBwrapper($conf);
+      $infoArray = $myObj->getData();
+      if(!isset($infoArray))echo "Nothing to be shown!";
+        else 
+             {
+               // The array has a content 
+               foreach($infoArray as $customer)
+               {
+                  echo $customer['id']." ".$customer['name']." ".$customer['email']."<br/>";
+               }
+             }
 
 ?>
